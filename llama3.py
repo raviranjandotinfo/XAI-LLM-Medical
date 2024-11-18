2e8f5da6b68e2efbd398da567325138ccfd0efd0
raviranjandotinfo <helloravi@live.com>
2024-11-18 11:28:13 -0500

    Create llama3.py

---
+++ llama3.py
@@ -0,0 +1,177 @@
+# Installing required liabaries
+%%capture
+# Installs Unsloth, Xformers (Flash Attention) and all other packages!
+!pip install unsloth
+# Get latest Unsloth
+!pip install --upgrade --no-deps "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+
+# Importing model and setiing up parametrs
+from unsloth import FastLanguageModel
+import torch
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+
+# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
+fourbit_models = [
+    "unsloth/mistral-7b-v0.3-bnb-4bit",      # New Mistral v3 2x faster!
+    "unsloth/mistral-7b-instruct-v0.3-bnb-4bit",
+    "unsloth/llama-3-8b-bnb-4bit",           # Llama-3 15 trillion tokens model 2x faster!
+    "unsloth/llama-3-8b-Instruct-bnb-4bit",
+    "unsloth/llama-3-70b-bnb-4bit",
+    "unsloth/Phi-3-mini-4k-instruct",        # Phi-3 2x faster!
+    "unsloth/Phi-3-medium-4k-instruct",
+    "unsloth/mistral-7b-bnb-4bit",
+    "unsloth/gemma-7b-bnb-4bit",             # Gemma 2.2x faster!
+] # More models at https://huggingface.co/unsloth
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/llama-3-8b-bnb-4bit",
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+
+  # Creating model and lora configurations
+  model = FastLanguageModel.get_peft_model(
+    model,
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    use_rslora = False,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
+)
+
+  # Importing datasets
+  from datasets import load_dataset
+dataset = load_dataset(
+    "csv",
+    data_files = "hf://datasets/QuyenAnhDE/Diseases_Symptoms/Diseases_Symptoms.csv",
+    split = "train",
+)
+print(dataset.column_names)
+print(dataset[0])
+
+# Creating dataset samples
+from unsloth import to_sharegpt
+dataset_simple = to_sharegpt(
+    dataset,
+    merged_prompt = "[[Patient symptoms are {Symptoms}.\n]]",
+    output_column_name = "Name",
+)
+
+from unsloth import to_sharegpt
+dataset = to_sharegpt(
+    dataset,
+    merged_prompt = \
+        "[[The Patient symptoms are {Symptoms}.]]",
+    conversation_extension = 5, # Randomnly combines conversations into 1! Good for long convos
+    output_column_name = "Name",
+)
+
+from pprint import pprint
+pprint(dataset[0])
+
+from unsloth import standardize_sharegpt
+dataset = standardize_sharegpt(dataset)
+
+# Tailoring I/O prompt and response
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+chat_template = """Below describes some details about some patient symptoms that they are experiencing.
+Predict the disease based on the symptoms.
+Output the disease name like Panic disorder, Choledocholithiasis, Chancroid, Abscess of the Lung, Mitral Valve Disease, extra.
+>>> Passenger Details:
+{INPUT}
+>>> Did they survive?
+{OUTPUT}"""
+
+from unsloth import apply_chat_template
+dataset = apply_chat_template(
+    dataset,
+    tokenizer = tokenizer,
+    chat_template = chat_template,
+    # default_system_message = "You are a helpful assistant", << [OPTIONAL]
+)
+
+chat_template = """{SYSTEM}
+USER: {INPUT}
+ASSISTANT: {OUTPUT}"""
+
+chat_template = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+{SYSTEM}<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+{INPUT}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+{OUTPUT}<|eot_id|>"""
+
+chat_template = """<|im_start|>system
+{SYSTEM}<|im_end|>
+<|im_start|>user
+{INPUT}<|im_end|>
+<|im_start|>assistant
+{OUTPUT}<|im_end|>"""
+
+# Creating SFTP trainer session
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from unsloth import is_bfloat16_supported
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    dataset_num_proc = 2,
+    packing = False, # Can make training 5x faster for short sequences.
+    args = TrainingArguments(
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 5,
+        max_steps = 60,
+        learning_rate = 2e-4,
+        fp16 = not is_bfloat16_supported(),
+        bf16 = is_bfloat16_supported(),
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        output_dir = "outputs",
+    ),
+)
+
+# Training the model 
+trainer_stats = trainer.train()
+
+# Evaluating finetined model
+FastLanguageModel.for_inference(model) # Enable native 2x faster inference
+messages = [                    # Change below!
+    {"role": "user", "content": 'Patient experiencing, Double vision, Eye fatigue, Poor depth perception, Head tilting. Also keep touching the eyes.'},
+]
+input_ids = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt = True,
+    return_tensors = "pt",
+).to("cuda")
+
+from transformers import TextStreamer
+text_streamer = TextStreamer(tokenizer, skip_prompt = True)
+_ = model.generate(input_ids, streamer = text_streamer, max_new_tokens = 128, pad_token_id = tokenizer.eos_token_id)
